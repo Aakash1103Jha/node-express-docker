0c0ec53ed61a245ae328c55ce57afc2ae45a2838
Aakash Jha <aakash.jha1103@gmail.com>
2022-08-21 11:53:21 +0530

    Added logging with Winston

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 require("dotenv/config");
 require("regenerator-runtime");
 
-import { app } from "./utils";
+import { transports, format } from "winston";
+
+import { app, logger } from "./utils";
 
 const PORT: string | number = process.env.PORT || 4000;
+const { NODE_ENV } = process.env;
 
 const server = app.listen(PORT, () => console.info(`Server running on port ${PORT}`));
+
+if (NODE_ENV !== "production") {
+	logger.add(
+		new transports.Console({
+			format: format.colorize({ colors: { info: "blue", error: "red" } }),
+		}),
+	);
+}
 
 export default server;
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,4 +4,5 @@
  */
 export { default as app } from "./app";
 export { default as DBHelper } from "./database";
+export { default as logger } from "./logger";
 
