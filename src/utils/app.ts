1dc5895fc9d8191802b8dab10271a877f1a79945
Aakash Jha <aakash.jha1103@gmail.com>
2022-08-21 21:28:03 +0530

    Fixed issue where logs were not written into files

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { randomBytes } from "crypto";
+import logger from "./logger";
 
 const app: Application = express();
 
@@ -11,7 +12,10 @@ app.use(cors());
 app.disable("x-powered-by");
 
 app.get("/welcome", async (req: Request, res: Response) => {
-	res.status(200).json({ message: "This app is running inside a Docker container", randomId: randomBytes(10).toString("hex") });
+	const randomId = randomBytes(10).toString("hex");
+	const message = "This app is running inside a Docker container";
+	res.status(200).json({ message, randomId });
+	return logger().info(`Incoming IP: ${req.ip}`);
 });
 
 export default app;
--- src/utils/logger/prod.ts
+++ src/utils/logger/prod.ts
@@ -5,8 +5,9 @@ const prod = createLogger({
 	level: "debug",
 	format: format.combine(format.timestamp(), format.errors({ stack: true }), format.json()),
 	transports: [
+		new transports.Console(),
 		new transports.File({ filename: "error.log", level: "error", dirname: resolve(__dirname, "..", "..", "logs") }),
-		new transports.File({ filename: "combined.log", level: "all", dirname: resolve(__dirname, "..", "..", "logs") }),
+		new transports.File({ filename: "combined.log", level: "debug", dirname: resolve(__dirname, "..", "..", "logs") }),
 	],
 });
 
